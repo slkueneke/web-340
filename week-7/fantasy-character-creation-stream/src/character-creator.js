dec575639b1c2c27d961166af0b40ad6e3dc62ea
Shannon Kueneke <slkueneke@gmail.com>
2025-12-02 15:58:17 -0800

    week 7 assignments

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,18 +1,66 @@
-const { Duplex } = require('stream');
+"use strict";
+
+const { Duplex } = require("stream");
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
-    // TODO: Initialize your class here
+    this.answers = [];
+    this.char = [];
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    // Convert input to string and split by commas
+    const parts = chunk
+      .toString()
+      .toLowerCase()
+      .split(",")
+      .map((p) => p.trim());
+    const [charClass, gender, funFact] = parts;
+    
+
+    if (!!charClass && charClass.length > 0 && charClass.match(/mage|warrior|rogue/g)) {
+      this.answers.push(charClass);
+      this.char.push(`Your character class is a ${charClass}.`);
+    } else {
+      throw new Error("Your character class must be mage, warrior, or rogue.");
+    }
+
+    if (!!gender && gender.length > 0 && gender.match(/male|female|other/g)) {
+      this.answers.push(gender);
+      this.char.push(`Your gender is ${gender}.`);
+    } else {
+      throw new Error("Your character gender must be male, female, or other.");
+    }
+
+    if (!!funFact && funFact.length > 0) {
+      this.answers.push(funFact);
+      this.char.push(`Your fun fact is ${funFact}.`);
+    } else {
+      throw new Error("Please enter a fun fact.");
+    }
+
+    callback();
   }
 
   _read(size) {
-    // TODO: Implement your _read method here
+    while (this.char.length > 0) {
+      this.push(this.char.shift() + "\n");
+    }
+    this.push(null);
   }
+
+
+  
 }
+
+/*const charCreator = new CharacterCreator();
+let output = "";
+charCreator.on("data", (chunk) => {
+  console.log(chunk.toString());
+  output += chunk;
+});
+
+charCreator.write("mage, female, cool");*/
 
 module.exports = CharacterCreator;
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,6 +1,6 @@
-const CharacterCreator = require('../src/character-creator');
+const CharacterCreator = require("../src/character-creator");
 
-describe('CharacterCreator', () => {
+describe("CharacterCreator", () => {
   let characterCreator;
 
   beforeEach(() => {
@@ -8,14 +8,35 @@ describe('CharacterCreator', () => {
   });
 
   test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+    characterCreator.on("finish", () => {
+      expect(characterCreator.answers).toEqual(["mage", "female", "super fun"]);
+      done();
+    });
+
+    characterCreator.write("mage, female, super fun");
+    characterCreator.end();
   });
 
-  test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+  test("should emit 'error' when invalid data is written", () => {
+    expect(() => {
+      characterCreator.write("mage, female"); // missing fun fact
+    }).toThrow("Please enter a fun fact.");
   });
 
   test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+    let output = "";
+    characterCreator.on("data", (chunk) => {
+      output += chunk.toString();
+    });
+
+    characterCreator.on("end", () => {
+      expect(output).toEqual(
+        "Your character class is a mage.\nYour gender is female.\nYour fun fact is super fun.\n"
+      );
+      done();
+    });
+
+    characterCreator.write("mage, female, super fun");
+    characterCreator.end();
   });
 });
